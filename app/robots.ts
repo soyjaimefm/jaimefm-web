d358fde21fa050db7c65b978e3209f9364015653
Jaime Fernández Moreno <jaimefrm93@gmail.com>
2024-04-27 11:22:26 +0200

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://jaimefm.dev/sitemap.xml',
+    sitemap: 'https://www.jaimefm.dev/sitemap.xml',
   }
 }
